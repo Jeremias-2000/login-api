9038187f106747525b3753610550afd8f2c7da5c
Jeremias <jdossantospinheiro20@gmail.com>
2021-04-29 14:52:56 -0300

    construct

---
+++ src/main/java/com/example/demo/controller/Requests.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+public interface Requests {
+
+    @GetMapping("/")
+    String test();
+
+    @GetMapping("/users")
+    ResponseEntity<?> getAll();
+
+    @GetMapping("/user/{id}")
+    ResponseEntity<?> getUser(@PathVariable("id") String id);
+
+    @GetMapping("/user/email/{email}")
+    ResponseEntity<?> getEmail(@PathVariable("email") String email);
+
+    @PostMapping("/Sign up")
+    void save(@Validated @RequestBody User user);
+
+    @PutMapping("update/{id}")
+    ResponseEntity<?> updateUser(@PathVariable("id") String id,@Validated @RequestBody User user);
+
+
+    @DeleteMapping("delete user/{id}")
+    ResponseEntity<?> deleteUser(@PathVariable("id") String id);
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mongodb.lang.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    private String email;
+    private String password;
+
+
+}
